f3a37865c0e7d66fb0b314c65e2d0612a770574e
DavidMitsko <david.mitko@mail.ru>
2020-10-25 14:40:31 +0300

    Added new methods for getting student`s logs with filters and time period, for setting teacher`s account and for customize reports

--- src/api/app.js
+++ src/api/app.js
@@ -6,7 +6,13 @@ import {
     RAISE_HAND,
     GET_AUTHORIZED_STUDENTS,
     GET_ALL_STUDENTS,
-    GET_STUDENTS_LOGS
+    GET_STUDENTS_LOGS,
+    GET_STUDENTS_LOGS_FILTER,
+    CHANGE_EMAIL,
+    REPORT_INFO,
+    TURN_OFF_REPORTS,
+    TURN_ON_REPORTS,
+    SET_UP_REPORTS
 } from "../constants";
 
 axios.defaults.baseURL = API_URL;
@@ -21,7 +27,21 @@ const getAuthorizedStudents = () => axios.get(GET_AUTHORIZED_STUDENTS)
 
 const getAllStudents = () => axios.get(GET_ALL_STUDENTS)
 
-const getStudentsLogs = studentId => axios.get(GET_STUDENTS_LOGS, {params: {studentId: studentId}})
+const getAllStudentsLogs = (studentId, time) => axios.get(GET_STUDENTS_LOGS,
+    {params: {studentId, time}})
+
+const getStudentsLogs = (actions, studentId, time) => axios.get(GET_STUDENTS_LOGS_FILTER,
+    {params: {actions, studentId, time}})
+
+const changeEmail = data => axios.post(CHANGE_EMAIL, data)
+
+const getReportInfo = userId => axios.get(REPORT_INFO, {params: {userId}})
+
+const turnOnReports = data => axios.post(TURN_ON_REPORTS, data);
+
+const turnOffReports = userId => axios.post(TURN_OFF_REPORTS, null, {params: {userId}});
+
+const setUpReports = data => axios.post((SET_UP_REPORTS, data));
 
 export const api = {
     signIn,
@@ -29,5 +49,11 @@ export const api = {
     raiseHand,
     getAuthorizedStudents,
     getAllStudents,
-    getStudentsLogs
+    getAllStudentsLogs,
+    getStudentsLogs,
+    changeEmail,
+    getReportInfo,
+    turnOffReports,
+    turnOnReports,
+    setUpReports
 }
